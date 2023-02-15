5f8e39bad8ca6c82a3ee224494626bb29b7132c9
awesome-tofu <adityaraj6311@gmail.com>
2023-02-15 23:56:59 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import axios from 'axios';
+import qs from 'qs';
+
+
+const app=express();
+
+
+
+app.get('/',(req,res)=>{
+
+
+const lang=req.query.language;
+const code=req.query.code;
+var data = qs.stringify({
+    'code': code,
+    'language': lang,
+    'input': ''
+});
+var config = {
+    method: 'post',
+    url: 'https://api.codex.jaagrav.in',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    data : data
+};
+
+axios(config)
+  .then(function (response) {
+    if (response.data.output==""){ res.send({output:"\nPlease Input Valid language\n",language:lang})}else{
+    res.send({output:"\n" + response.data.output,language:lang,status:response.data.status});
+}
+  })
+  .catch(function (error) {
+    console.log(error);
+    const err={output:error.response.data.error,language:lang,status:error.response.data.status};
+    res.send(err);
+  });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => console.log('You App is ACTIVE!🎊 at http://localhost:' + PORT));
